77b65baac93b64e3d2edbce4dd97be9057feef5a
Rob Beckmann <rob.beckmann.dev@gmail.com>
2022-04-29 20:56:15 -0500

    Add additional timeout error handling.  Appears a slightly different connection error type can get raised in some cases.

--- main.py
+++ main.py
@@ -13,17 +13,7 @@ class StreamLootsMonitor:
 
     url_base = "https://widgets.streamloots.com/alerts/"
     NEXT_LIST_ERR_SMALL = "Error updating Next List, check logs when possible."
-    @classmethod
-    def initiate_connection(cls):
-        try:
-            sess = requests.Session()
-            streamloots_id = App.config().main.streamloots_id
-            url = f"{cls.url_base}{streamloots_id}/media-stream"
-            req = requests.Request("GET", url).prepare()
-            return sess.send(req, stream=True)
-        except Exception as err:
-            sess.close()
-            logger.error(err)
+    TIMEOUT_ERR = "No data received from feed within timeout period, reconnecting..."
 
     @staticmethod
     def get_username(data):
@@ -58,8 +48,8 @@ class StreamLootsMonitor:
                 # Don't bother with error itself, failure already recorded.
                 print(cls.NEXT_LIST_ERR_SMALL)
                 logger.error(
-                    f"Failed to update NextList - raw game id: {segment}, "
-                    f"votes: {vote_string}, full message: {data}"
+                    f"Failed to update NextList - raw game id: {game_id_segment}, "
+                    f"votes: {votes}, full message: {data}"
                 )
             except Exception as err:
                 print(cls.NEXT_LIST_ERR_SMALL)
@@ -75,52 +65,80 @@ class StreamLootsMonitor:
     @classmethod
     def parse_message(cls, info):
         if data := info.get('data'):
-            desc = data.get('description')
-            if desc is None:
-                # description not included for messages about users buying cards
-                # or updating subcriptions.
-                return
-            if 'nextlist' in desc.replace(" ","").lower():
-                cls._handle_next_list(desc, data)
+
+            if desc := data.get("description"):
+                if 'nextlist' in desc.replace(" ","").lower():
+                    cls._handle_next_list(desc, data)
+            else:
+                username = data.get("message")
+                settings = data.get("settings")
+                sub_alert = False
+                if settings:
+                    sdata = settings.get("data")
+                    if sdata:
+                        sub_alert = True if sdata.get("isSubscriptionAlert") else False
+                if sub_alert:
+                    print(f"Sub alert for {username}")
 
     @classmethod
     def execute(cls):
         print("Initializing connection to Streamloots...")
-        data_stream = cls.initiate_connection()
-        if not data_stream:
-            print("Failed to establish connection")
-            return 
+        
+        streamloots_id = App.config().main.streamloots_id
+        url = f"{cls.url_base}{streamloots_id}/media-stream"
+        # Streamloots updates are sent every 30 seconds if no active message is
+        # sent.
+        # If we a make it a full minute with no message, timeout and reconnect.
+        with requests.get(url, stream=True, timeout=60) as data_stream:
+            if not data_stream:
+                print("Failed to establish connection")
+                return
 
-        print("Connection to Streamloots data stream successful.")
-        for line in data_stream.iter_lines():
-            if line:
-                if decoded := line.decode("utf-8"):
-                    # Streamloots string usually starts with "data: "
-                    # For some reason this is not included inside a standard set of curly
-                    # braces even though the rest of it is valid json.
-                    decoded = decoded[6:] if len(decoded) > 6 else decoded
-                    try:
-                        if decoded.strip() == ":":
-                            # empty data that just populates every so often
-                            continue
-                        logger.debug(decoded)
-                        cls.parse_message(json.loads(decoded))
-                    except Exception as err:
-                        logger.error(f"Error {str(err)} parsing streamloots message: {decoded}")
+            print("Connection to Streamloots data stream successful.")
+            App.config().connection_attempts = 0
+            for line in data_stream.iter_lines():
+                if line:
+                    if decoded := line.decode("utf-8"):
+                        # Streamloots string usually starts with "data: "
+                        # For some reason this is not included inside a standard set of curly
+                        # braces even though the rest of it is valid json.
+                        decoded = decoded[6:] if len(decoded) > 6 else decoded
+                        try:
+                            if decoded.strip() == ":":
+                                # empty data that just populates every so often
+                                continue
+                            logger.debug(decoded)
+                            cls.parse_message(json.loads(decoded))
+                        except Exception as err:
+                            logger.error(f"Error {str(err)} parsing streamloots message: {decoded}")
 
     @classmethod
     def execute_with_retry(cls):
-        retries = App.config().main.get('max_retries') or 3
-        for i in range(retries):
+       
+        max_retries = App.config().main.get('max_retries') or 3
+        reconnect_wait = App.config().main.get('connection_retry_wait_seconds') or 10
+        App.config().connection_attempts = 0
+
+        while App.config().connection_attempts < max_retries:
             try:
+                App.config().connection_attempts += 1
                 cls.execute()
             except requests.exceptions.ConnectionError as err:
+                if 'Read timed out' in str(err):
+                    print(cls.TIMEOUT_ERR)
+                else:
+                    print("Unexpected connection error with Streamloots, reconnecting...")
                 logger.error(f"Error with Streamloots Connection: {err}")
+            except requests.exceptions.ReadTimeout as err:
+                print(cls.TIMEOUT_ERR)
+                logger.error(str(err))
             except socket.timeout:
                 logger.error(f"Socket timeout to Streamloots")
-            time.sleep(App.config().main.get('connection_retry_wait_seconds') or 10)
+            except Exception as err:
+                logger.error(f"Unknown error - reconnecting: {err}")
+            time.sleep(reconnect_wait)
         else:
-            print("Unable to connect to Streamloots after {retries} attempts.")
+            print(f"Unable to connect to Streamloots after {max_retries} attempts.")
 
 
 StreamLootsMonitor.execute_with_retry()
